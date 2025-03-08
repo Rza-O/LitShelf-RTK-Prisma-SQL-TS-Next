d5e590cda7bd3ed708f2a00f2488aae2ebcbe9c4
Rza-O <shahreza.dev@gmail.com>
2025-03-08 08:33:07 +0600

    Update API URL in Home component to use environment variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default async function Home() {
-  const { data } = await axios("http://localhost:3000/api/books");
+  const { data } = await axios(`${process.env.NEXT_PUBLIC_API_URL}/api/books`);
   console.log(data);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
